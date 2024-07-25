31611d758da3e11721dc06dfa1f649c89bc45a2e
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2024-07-25 16:44:44 +0330

    ترجمه و بهینه سازی Bind.php

--- app/Plugins/Telegram/Commands/Bind.php
+++ app/Plugins/Telegram/Commands/Bind.php
@@ -7,32 +7,32 @@ use App\Plugins\Telegram\Telegram;
 
 class Bind extends Telegram {
     public $command = '/bind';
-    public $description = '将Telegram账号绑定到网站';
+    public $description = 'لطفاً‌اکانت‌تلگرام‌را‌به‌وب‌سایت‌متصل‌کنید';
 
     public function handle($message, $match = []) {
         if (!$message->is_private) return;
         if (!isset($message->args[0])) {
-            abort(500, '参数有误，请携带订阅地址发送');
+            abort(500, 'پارامتر اشتباه است⚠️ بعد از عبارت bind یک فاصل بزارید و لینک حساب خود را وارد و مجدد ارسال کنید.');
         }
         $subscribeUrl = $message->args[0];
         $subscribeUrl = parse_url($subscribeUrl);
         parse_str($subscribeUrl['query'], $query);
         $token = $query['token'];
         if (!$token) {
-            abort(500, '订阅地址无效');
+            abort(500, 'آدرس‌اشتراک‌نامعتبراست');
         }
         $user = User::where('token', $token)->first();
         if (!$user) {
-            abort(500, '用户不存在');
+            abort(500, 'کاربروجودندارد');
         }
         if ($user->telegram_id) {
-            abort(500, '该账号已经绑定了Telegram账号');
+            abort(500, 'ربات از قبل به یک اکانت متصل شده است. برای قطع اتصال از عبارت /unbind استفاده کنید.');
         }
         $user->telegram_id = $message->chat_id;
         if (!$user->save()) {
-            abort(500, '设置失败');
+            abort(500, 'راه‌اندازی‌ناموفق‌بود');
         }
         $telegramService = $this->telegramService;
-        $telegramService->sendMessage($message->chat_id, '绑定成功');
+        $telegramService->sendMessage($message->chat_id, 'با موفقیت متصل شد😍 برای مشاهده ترافیک خود عبارت /traffic را ارسال کنید.');
     }
 }

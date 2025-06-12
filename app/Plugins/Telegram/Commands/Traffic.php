abecbfcfaa1df4273561083ea3c1224b782d9539
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2025-06-12 17:13:56 +0330

    Update Traffic.php

--- app/Plugins/Telegram/Commands/Traffic.php
+++ app/Plugins/Telegram/Commands/Traffic.php
@@ -8,21 +8,21 @@ use App\Utils\Helper;
 
 class Traffic extends Telegram {
     public $command = '/traffic';
-    public $description = '查询流量信息';
+    public $description = 'استعلام اطلاعات ترافیک';
 
     public function handle($message, $match = []) {
         $telegramService = $this->telegramService;
         if (!$message->is_private) return;
         $user = User::where('telegram_id', $message->chat_id)->first();
         if (!$user) {
-            $telegramService->sendMessage($message->chat_id, '没有查询到您的用户信息，请先绑定账号', 'markdown');
+            $telegramService->sendMessage($message->chat_id, 'اطلاعات کاربری شما یافت نشد، لطفاً ابتدا حساب خود را متصل کنید', 'markdown');
             return;
         }
         $transferEnable = Helper::trafficConvert($user->transfer_enable);
         $up = Helper::trafficConvert($user->u);
         $down = Helper::trafficConvert($user->d);
         $remaining = Helper::trafficConvert($user->transfer_enable - ($user->u + $user->d));
-        $text = "🚥流量查询\n———————————————\n计划流量：`{$transferEnable}`\n已用上行：`{$up}`\n已用下行：`{$down}`\n剩余流量：`{$remaining}`";
+        $text = "🚥استعلام ترافیک\n———————————————\nترافیک پلن: `{$transferEnable}`\nآپلود مصرفی: `{$up}`\nدانلود مصرفی: `{$down}`\nترافیک باقیمانده: `{$remaining}`";
         $telegramService->sendMessage($message->chat_id, $text, 'markdown');
     }
 }

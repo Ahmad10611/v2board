4ed3a8311f8ba8817d875d5553caf8a3a1b37670
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2025-07-05 19:46:39 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use App\Services\ThemeService;
 use Illuminate\Http\Request;
 use App\Http\Controllers\V1\Guest\PaymentController;
+use Illuminate\Support\Facades\Cache;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function (Request $request) {
@@ -21,13 +17,16 @@ Route::get('/', function (Request $request) {
             abort(403);
         }
     }
-    $renderParams = [
-        'title' => config('v2board.app_name', 'V2Board'),
-        'theme' => config('v2board.frontend_theme', 'default'),
-        'version' => config('app.version'),
-        'description' => config('v2board.app_description', 'V2Board is best'),
-        'logo' => config('v2board.logo')
-    ];
+    
+    $renderParams = Cache::remember('v2board_render_params', 3600, function() {
+        return [
+            'title' => config('v2board.app_name', 'V2Board'),
+            'theme' => config('v2board.frontend_theme', 'default'),
+            'version' => config('app.version'),
+            'description' => config('v2board.app_description', 'V2Board is best'),
+            'logo' => config('v2board.logo')
+        ];
+    });
 
     if (!config("theme.{$renderParams['theme']}")) {
         $themeService = new ThemeService($renderParams['theme']);
@@ -38,7 +37,6 @@ Route::get('/', function (Request $request) {
     return view('theme::' . config('v2board.frontend_theme', 'default') . '.dashboard', $renderParams);
 });
 
-//TODO:: 兼容
 Route::get('/' . config('v2board.secure_path', config('v2board.frontend_admin_path', hash('crc32b', config('app.key')))), function () {
     return view('admin', [
         'title' => config('v2board.app_name', 'V2Board'),
@@ -55,7 +53,13 @@ Route::get('/' . config('v2board.secure_path', config('v2board.frontend_admin_pa
 if (!empty(config('v2board.subscribe_path'))) {
     Route::get(config('v2board.subscribe_path'), 'V1\\Client\\ClientController@subscribe')->middleware('client');
 }
+
+// مسیرهای پرداخت
+Route::post('payment/notify/{method}/{uuid}', [PaymentController::class, 'notify'])
+    ->name('payment.notify')
+    ->middleware('throttle:60,1');
+
+// مسیرهای legacy
 Route::post('/api/v1/guest/payment/callback/aghayehpardakht', [PaymentController::class, 'aghayehpardakhtCallback']);
-Route::post('payment/notify/{method}/{uuid}', [PaymentController::class, 'notify'])->name('payment.notify');
 Route::post('/api/v1/guest/payment/callback/zibal', [PaymentController::class, 'zibalCallback']);
 Route::post('payment/notify/zibal/{uuid}', [PaymentController::class, 'notify'])->name('payment.notify.zibal');

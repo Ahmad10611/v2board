c32f792278ddba1a91e7cbd7ff6027e420a8b072
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2025-09-03 15:15:29 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Services\ThemeService;
 use Illuminate\Http\Request;
+use App\Http\Controllers\V1\Guest\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -54,3 +55,12 @@ Route::get('/' . config('v2board.secure_path', config('v2board.frontend_admin_pa
 if (!empty(config('v2board.subscribe_path'))) {
     Route::get(config('v2board.subscribe_path'), 'V1\\Client\\ClientController@subscribe')->middleware('client');
 }
+// مسیرهای پرداخت
+Route::post('payment/notify/{method}/{uuid}', [PaymentController::class, 'notify'])
+    ->name('payment.notify')
+    ->middleware('throttle:60,1');
+
+// مسیرهای legacy
+Route::post('/api/v1/guest/payment/callback/aghayehpardakht', [PaymentController::class, 'aghayehpardakhtCallback']);
+Route::post('/api/v1/guest/payment/callback/zibal', [PaymentController::class, 'zibalCallback']);
+Route::post('payment/notify/zibal/{uuid}', [PaymentController::class, 'notify'])->name('payment.notify.zibal');

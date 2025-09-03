0731547148305d79fa32e139b8ad82b62d8a1598
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2025-09-03 15:45:54 +0330

    Update UnBind.php

--- app/Plugins/Telegram/Commands/UnBind.php
+++ app/Plugins/Telegram/Commands/UnBind.php
@@ -7,20 +7,22 @@ use App\Plugins\Telegram\Telegram;
 
 class UnBind extends Telegram {
     public $command = '/unbind';
-    public $description = '将Telegram账号从网站解绑';
+    public $description = 'جداسازی حساب تلگرام از وب‌سایت';
 
     public function handle($message, $match = []) {
         if (!$message->is_private) return;
         $user = User::where('telegram_id', $message->chat_id)->first();
         $telegramService = $this->telegramService;
         if (!$user) {
-            $telegramService->sendMessage($message->chat_id, '没有查询到您的用户信息，请先绑定账号', 'markdown');
+            $telegramService->sendMessage($message->chat_id, 'اطلاعات کاربری شما یافت نشد، لطفاً ابتدا حساب خود را متصل کنید', 'markdown');
             return;
         }
         $user->telegram_id = NULL;
         if (!$user->save()) {
-            abort(500, '解绑失败');
+            abort(500, 'جداسازی ناموفق بود');
         }
-        $telegramService->sendMessage($message->chat_id, '解绑成功', 'markdown');
+        $telegramService->sendMessage($message->chat_id, 'جداسازی با موفقیت انجام شد', 'markdown');
     }
 }
+
+?>
